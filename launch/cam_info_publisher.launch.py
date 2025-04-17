5d41c519c329444f5bb7cd1caf3daf1af1a730b4
adwaitnaik97 <adwait@aidrivers.ai>
2025-04-17 13:51:52 +0100

    Reapply .gitignore rules

--- launch/cam_info_publisher.launch.py
+++ launch/cam_info_publisher.launch.py
@@ -1,12 +1,24 @@
+import os
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
+    # Define the path to the specific YAML file
+    config_file_path = '/home/adwait/workspace/ros2_packages/cam_info_publisher/config/8381/18481922.yaml'
+
     return LaunchDescription([
+        DeclareLaunchArgument(
+            'camera_config', default_value=config_file_path,
+            description='Path to the camera configuration file'
+        ),
+
         Node(
-            package='cam_info_publisher',
-            executable='cam_info_publisher',
-            name='camera_info_pub',
-            output='screen'
-        )
+            package='cam_info_publisher',                # The ROS 2 package
+            executable='cam_info_publisher',             # The compiled executable name
+            name='cam_info_publisher',                # Node name (can be the same as executable)
+            output='screen',                             # Output to the terminal
+            parameters=[config_file_path]                # Load the YAML file as parameters
+        ),
     ])
